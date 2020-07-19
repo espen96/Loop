243b5c577f448a72cf9708c143bc7855c404a97e
Espen <espen96@gmail.com>
2020-07-19 12:26:27 +0200

    change of AO

--- shaders/program/gbuffers/terrain.glsl
+++ shaders/program/gbuffers/terrain.glsl
@@ -441,7 +441,7 @@ float shading = 1.0;
         ao = texture2D(normals, lmtexcoord.xy).z*2.0-1.;
 		#endif
 		vec3 diffuseLight = direct + texture2D(gaux1,(lmtexcoord.zw*15.+0.5)*texelSize).rgb;
-		vec3 color = color.rgb*(clamp(clamp(diffuseLight,0,1)*ao,0,1));	
+		vec3 color = (color.rgb*ao)*(clamp(clamp(diffuseLight,0,1),0,1));	
 	
 	
 	
